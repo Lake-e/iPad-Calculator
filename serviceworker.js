520adee73e0b9a900f87751c937ddbe7a8c0453e
Lake-e <95671554+Lake-e@users.noreply.github.com>
2022-11-09 21:54:15 -0500

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -10,3 +10,14 @@ self.addEventListener('install', event => {
       )
   )
 })
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) {
+        return response
+      }
+      return fetch(event.request)
+    })
+  )
+})
